3d7ccf9474e6a1d02f9911bb57f104deeb204520
Lim Shang Yi <shangyilim@gmail.com>
2019-06-06 11:32:46 +0800

    prerender exclusions

--- src/components/gallery_block/index.js
+++ src/components/gallery_block/index.js
@@ -1,60 +1,107 @@
 /**
  * Copyright 2016 Oleh Zasadnyy, GDG Lviv
  * Source: https://github.com/gdg-x/hoverboard
-*/
+ */
 
-import { Component } from 'preact';
-import style from './style';
+import { Component } from "preact";
+import style from "./style";
 
 export default class GalleryBlock extends Component {
+  constructor(props) {
+    super(props);
+    if (typeof window !== "undefined") {
+      this.io = new IntersectionObserver(
+        entries => {
+          const visibleEntries = entries.filter(e => e.isIntersecting);
 
-	constructor(props){
-		super(props);
+          visibleEntries
+            .filter(e => e.target instanceof HTMLImageElement)
+            .forEach(e => {
+              e.target.src = e.target.dataset.src;
+            });
+        },
+        {
+          /* Using default options. Details below */
+        }
+      );
+    }
+  }
 
-		this.io = new IntersectionObserver(
-			entries => {
-				const visibleEntries = entries.filter(e=> e.isIntersecting);
-				
-				visibleEntries.filter(e=> e.target instanceof HTMLImageElement).forEach((e)=> {
-					e.target.src = e.target.dataset.src;
-				})
-			},
-			{
-				/* Using default options. Details below */
-			}
-		);
-	}
+  componentDidMount() {
+    if (!this.io) return;
 
-	componentDidMount(){
-		const elements = document.querySelectorAll(`.${style.grid_item}`);
+    const elements = document.querySelectorAll(`.${style.grid_item}`);
 
-		elements.forEach(element=> this.io.observe(element));
-	}
+    elements.forEach(element => this.io.observe(element));
+  }
 
-	componentWillUnmount(){
+  componentWillUnmount() {
+    if (!this.io) return;
 
-		this.io.disconnect();
-	}
+    this.io.disconnect();
+  }
 
-	render() {
-		return (
-			<div class={style.photos_grid}>
-				<img crossorigin="anonymous" class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072861/devfestkl18_pwa/gallery/1.jpg" />
-				<img crossorigin="anonymous" class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072831/devfestkl18_pwa/gallery/2.jpg" />
-				<img crossorigin="anonymous" class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072843/devfestkl18_pwa/gallery/3.jpg" />
-				<img crossorigin="anonymous" class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072857/devfestkl18_pwa/gallery/4.jpg" />
-				<img crossorigin="anonymous" class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072841/devfestkl18_pwa/gallery/5.jpg" />
-				<img crossorigin="anonymous" class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072846/devfestkl18_pwa/gallery/6.jpg" />
-				<img crossorigin="anonymous" class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072883/devfestkl18_pwa/gallery/7.jpg" />
-				<img crossorigin="anonymous" class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072866/devfestkl18_pwa/gallery/8.jpg" />
-				<div crossorigin="anonymous" class={style.gallery_info}>
-					<div>
-						<h2>I/O Extended KL 2018 highlights</h2>
-						<p>This year's festival built lots of excitement. Check out photos from featured talks, hands-on learning sessions, and after-hours fun.</p>
-					</div>
-					<a href="https://photos.app.goo.gl/uw6aRmAurGVjafrt8" target="_blank" rel="noopener noreferrer">See all photos</a>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div class={style.photos_grid}>
+        <img
+          crossorigin="anonymous"
+          class={style.grid_item}
+          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072861/devfestkl18_pwa/gallery/1.jpg"
+        />
+        <img
+          crossorigin="anonymous"
+          class={style.grid_item}
+          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072831/devfestkl18_pwa/gallery/2.jpg"
+        />
+        <img
+          crossorigin="anonymous"
+          class={style.grid_item}
+          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072843/devfestkl18_pwa/gallery/3.jpg"
+        />
+        <img
+          crossorigin="anonymous"
+          class={style.grid_item}
+          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072857/devfestkl18_pwa/gallery/4.jpg"
+        />
+        <img
+          crossorigin="anonymous"
+          class={style.grid_item}
+          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072841/devfestkl18_pwa/gallery/5.jpg"
+        />
+        <img
+          crossorigin="anonymous"
+          class={style.grid_item}
+          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072846/devfestkl18_pwa/gallery/6.jpg"
+        />
+        <img
+          crossorigin="anonymous"
+          class={style.grid_item}
+          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072883/devfestkl18_pwa/gallery/7.jpg"
+        />
+        <img
+          crossorigin="anonymous"
+          class={style.grid_item}
+          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072866/devfestkl18_pwa/gallery/8.jpg"
+        />
+        <div crossorigin="anonymous" class={style.gallery_info}>
+          <div>
+            <h2>I/O Extended KL 2018 highlights</h2>
+            <p>
+              This year's festival built lots of excitement. Check out photos
+              from featured talks, hands-on learning sessions, and after-hours
+              fun.
+            </p>
+          </div>
+          <a
+            href="https://photos.app.goo.gl/uw6aRmAurGVjafrt8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            See all photos
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,254 +1,265 @@
-import { Component } from 'preact';
-import IoLogo from '../../components/io_logo';
-import SocialFooter from '../../components/social_footer';
-import Footer from '../../components/footer';
-import GalleryBlock from '../../components/gallery_block';
-import Countdown from '../../components/Countdown';
-import style from './style';
+import { Component } from "preact";
+import IoLogo from "../../components/io_logo";
+import SocialFooter from "../../components/social_footer";
+import Footer from "../../components/footer";
+import GalleryBlock from "../../components/gallery_block";
+import Countdown from "../../components/Countdown";
+import style from "./style";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    if (typeof window !== "undefined") {
+      this.io = new IntersectionObserver(
+        entries => {
+          const visibleEntries = entries.filter(e => e.isIntersecting);
 
-	constructor(props){
-		super(props);
+          visibleEntries
+            .filter(e => e.target instanceof HTMLImageElement)
+            .forEach(e => {
+              e.target.src = e.target.dataset.src;
+            });
+        },
+        {
+          /* Using default options. Details below */
+        }
+      );
+    }
+  }
+  handleScroll() {
+    const ele = document.querySelector(".topappbar.mdc-top-app-bar");
+    if (document.documentElement.scrollTop < 56) {
+      ele.setAttribute("top", true);
+    } else {
+      ele.removeAttribute("top");
+    }
+  }
 
-		this.io = new IntersectionObserver(
-			entries => {
-				const visibleEntries = entries.filter(e=> e.isIntersecting);
-				
-				visibleEntries.filter(e=> e.target instanceof HTMLImageElement).forEach((e)=> {
-					e.target.src = e.target.dataset.src;
-				})
-			},
-			{
-				/* Using default options. Details below */
-			}
-		);
-	}
-	handleScroll() {
-		const ele = document.querySelector('.topappbar.mdc-top-app-bar');
-		if (document.documentElement.scrollTop < 56) {
-			ele.setAttribute('top', true);
-		}
-		else {
-			ele.removeAttribute('top');
-		}
-	}
+  componentDidMount() {
+    document.title = "I/O Extended 2019 Kuala Lumpur";
+    window.addEventListener("scroll", this.handleScroll, { passive: true });
+    this.handleScroll();
 
-	componentDidMount() {
-		document.title = 'I/O Extended 2019 Kuala Lumpur';
-		window.addEventListener('scroll', this.handleScroll, { passive: true });
-		this.handleScroll();
-		
+    const ele = document.querySelector(".belt");
+    const cover = document.querySelector("#cover");
+    const sponsorLogos = document.querySelectorAll(".sponsor_logo");
 
-		const ele = document.querySelector('.belt');
-		const cover = document.querySelector('#cover');
-		const sponsorLogos = document.querySelectorAll('.sponsor_logo');
-		this.io.observe(ele);
-		this.io.observe(cover);
-		sponsorLogos.forEach(logo => this.io.observe(logo));
-	}
+    if (!this.io) return;
 
-	componentWillUnmount() {
-		window.removeEventListener('scroll', this.handleScroll);
-		document.querySelector('.topappbar.mdc-top-app-bar').removeAttribute('top');
-		this.io.disconnect();
-	}
+    this.io.observe(ele);
+    this.io.observe(cover);
+    sponsorLogos.forEach(logo => this.io.observe(logo));
+  }
 
-	render({ rootPath, partners }) {
-		return (
-			<div>
-				<div class={`${style.hero} hero`}>
-					<div class={style.hero_title}>
-						<IoLogo rootPath={rootPath} />
-						<h2>
-							I/O brings together developers from around the globe for talks, hands-on learning with Google experts, and a first look at Google’s latest developer products.
-						</h2>
-						<br />
-						<h4>16 June 2019 · Sunway University, Bandar Sunway</h4>
-					</div>
-					<Countdown />
-				</div>
-				<div class={`${style.belt} belt`}>
-					<img id="cover" crossorigin="anonymous" data-src="https://res.cloudinary.com/limhenry/image/upload/v1536157604/devfestkl18_pwa/misc/cover.jpg" />
-				</div>
-				<div class={style.home_info}>
-					<div class={style.text}>
-						<h3>What you need to know, before you ask.</h3>
-						<p>
-							I/O Extended 2019 Kuala Lumpur brings together the world class experts
-							in Android, Web, Machine Learning and Cloud technologies for one full day of
-							sessions, workshops and showcases.
-						</p>
-					</div>
-					<div class={style.stats}>
-						<div class={style.stat}>
-							<div class={style.number}>450+</div>
-							<div class={style.label}>Attendees</div>
-						</div>
-						<div class={style.stat}>
-							<div class={style.number}>1</div>
-							<div class={style.label}>Day</div>
-						</div>
-					</div>
-				</div>
-				<GalleryBlock />
-				{partners && (
-					<div class={style.partners}>
-						<h3>Partners</h3>
-						<h4>Main Partner of 2019</h4>
-						{partners.main_partner && (
-										<a
-											class={style.item}
-											href={partners.main_partner.url}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<img
-												crossorigin="anonymous"
-												class="sponsor_logo"
-												data-src={partners.main_partner.image}
-												alt={partners.main_partner.name}
-											/>
-										</a>)
-						}
-						<h4>Community Partners</h4>
-						{partners.partner && (
-							<div class={style.partner}>
-								<div class={style.sponsor}>
-									{partners.partner.map(item => (
-										<a
-											class={style.item}
-											href={item.url}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<img
-												class="sponsor_logo"
-												crossorigin="anonymous"
-												data-src={item.image}
-												alt={item.name}
-											/>
-										</a>
-									))}
-								</div>
-							</div>
-						)}
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+    document.querySelector(".topappbar.mdc-top-app-bar").removeAttribute("top");
 
-						{partners.general_sponsor && (
-							<div class={style.partner}>
-								<h4>Our Mind-blowing Gold Sponsors</h4>
-								<div class={style.sponsor}>
-									{partners.general_sponsor.map(item => (
-										<a
-											class={style.item}
-											href={item.url}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<img
-												class="sponsor_logo"
-												crossorigin="anonymous"
-												data-src={item.image}
-												alt={item.name}
-											/>
-										</a>
-									))}
-								</div>
-							</div>
-						)}
-						{partners.sponsors && (
-							<div class={style.partner}>
-								<h4>Our Awesome Silver Sponsors</h4>
-								<div class={style.sponsor}>
-									{partners.sponsors.map(item => (
-										<a
-											class={style.item}
-											href={item.url}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<img
-												class="sponsor_logo"
-												crossorigin="anonymous"
-												data-src={item.image}
-												alt={item.name}
-											/>
-										</a>
-									))}
-								</div>
-							</div>
-						)}
-						{partners.community_sponsors && (
-							<div class={style.partner}>
-								<h4>Our Hardcore Fans</h4>
-								<div class={style.sponsor}>
-									{partners.community_sponsors.map(item => (
-										<a
-											class={style.item}
-											href={item.url}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<img
-												class="sponsor_logo"
-												crossorigin="anonymous"
-												data-src={item.image}
-												alt={item.name}
-											/>
-										</a>
-									))}
-								</div>
-							</div>
-						)}
-						{partners.sponsors && (
-							<div class={style.partner}>
-								<h4>Official Ticketing Partner</h4>
-								<div class={style.sponsor}>
-									{partners.ticketing_partner.map(item => (
-										<a
-											class={style.item}
-											href={item.url}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<img
-												class="sponsor_logo"
-												crossorigin="anonymous"
-												data-src={item.image}
-												alt={item.name}
-											/>
-										</a>
-									))}
-								</div>
-							</div>
-						)}
-						{partners.organizers && (
-							<div class={style.partner}>
-								<h4>With Love From</h4>
-								<div class={style.sponsor}>
-									{partners.organizers.map(item => (
-										<a
-											class={style.item}
-											href={item.url}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<img
-												class="sponsor_logo"
-												crossorigin="anonymous"
-												data-src={item.image}
-												alt={item.name}
-											/>
-										</a>
-									))}
-								</div>
-							</div>
-						)}
-					</div>
-				)}
-				<SocialFooter rootPath={rootPath} />
-				<Footer rootPath={rootPath} />
-			</div>
-		);
-	}
+    if (!this.io) return;
+    this.io.disconnect();
+  }
+
+  render({ rootPath, partners }) {
+    return (
+      <div>
+        <div class={`${style.hero} hero`}>
+          <div class={style.hero_title}>
+            <IoLogo rootPath={rootPath} />
+            <h2>
+              I/O brings together developers from around the globe for talks,
+              hands-on learning with Google experts, and a first look at
+              Google’s latest developer products.
+            </h2>
+            <br />
+            <h4>16 June 2019 · Sunway University, Bandar Sunway</h4>
+          </div>
+          <Countdown />
+        </div>
+        <div class={`${style.belt} belt`}>
+          <img
+            id="cover"
+            crossorigin="anonymous"
+            data-src="https://res.cloudinary.com/limhenry/image/upload/v1536157604/devfestkl18_pwa/misc/cover.jpg"
+          />
+        </div>
+        <div class={style.home_info}>
+          <div class={style.text}>
+            <h3>What you need to know, before you ask.</h3>
+            <p>
+              I/O Extended 2019 Kuala Lumpur brings together the world class
+              experts in Android, Web, Machine Learning and Cloud technologies
+              for one full day of sessions, workshops and showcases.
+            </p>
+          </div>
+          <div class={style.stats}>
+            <div class={style.stat}>
+              <div class={style.number}>450+</div>
+              <div class={style.label}>Attendees</div>
+            </div>
+            <div class={style.stat}>
+              <div class={style.number}>1</div>
+              <div class={style.label}>Day</div>
+            </div>
+          </div>
+        </div>
+        <GalleryBlock />
+        {partners && (
+          <div class={style.partners}>
+            <h3>Partners</h3>
+            <h4>Main Partner of 2019</h4>
+            {partners.main_partner && (
+              <a
+                class={style.item}
+                href={partners.main_partner.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  crossorigin="anonymous"
+                  class="sponsor_logo"
+                  data-src={partners.main_partner.image}
+                  alt={partners.main_partner.name}
+                />
+              </a>
+            )}
+            <h4>Community Partners</h4>
+            {partners.partner && (
+              <div class={style.partner}>
+                <div class={style.sponsor}>
+                  {partners.partner.map(item => (
+                    <a
+                      class={style.item}
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        class="sponsor_logo"
+                        crossorigin="anonymous"
+                        data-src={item.image}
+                        alt={item.name}
+                      />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )}
+
+            {partners.general_sponsor && (
+              <div class={style.partner}>
+                <h4>Our Mind-blowing Gold Sponsors</h4>
+                <div class={style.sponsor}>
+                  {partners.general_sponsor.map(item => (
+                    <a
+                      class={style.item}
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        class="sponsor_logo"
+                        crossorigin="anonymous"
+                        data-src={item.image}
+                        alt={item.name}
+                      />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )}
+            {partners.sponsors && (
+              <div class={style.partner}>
+                <h4>Our Awesome Silver Sponsors</h4>
+                <div class={style.sponsor}>
+                  {partners.sponsors.map(item => (
+                    <a
+                      class={style.item}
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        class="sponsor_logo"
+                        crossorigin="anonymous"
+                        data-src={item.image}
+                        alt={item.name}
+                      />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )}
+            {partners.community_sponsors && (
+              <div class={style.partner}>
+                <h4>Our Hardcore Fans</h4>
+                <div class={style.sponsor}>
+                  {partners.community_sponsors.map(item => (
+                    <a
+                      class={style.item}
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        class="sponsor_logo"
+                        crossorigin="anonymous"
+                        data-src={item.image}
+                        alt={item.name}
+                      />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )}
+            {partners.sponsors && (
+              <div class={style.partner}>
+                <h4>Official Ticketing Partner</h4>
+                <div class={style.sponsor}>
+                  {partners.ticketing_partner.map(item => (
+                    <a
+                      class={style.item}
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        class="sponsor_logo"
+                        crossorigin="anonymous"
+                        data-src={item.image}
+                        alt={item.name}
+                      />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )}
+            {partners.organizers && (
+              <div class={style.partner}>
+                <h4>With Love From</h4>
+                <div class={style.sponsor}>
+                  {partners.organizers.map(item => (
+                    <a
+                      class={style.item}
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        class="sponsor_logo"
+                        crossorigin="anonymous"
+                        data-src={item.image}
+                        alt={item.name}
+                      />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+        <SocialFooter rootPath={rootPath} />
+        <Footer rootPath={rootPath} />
+      </div>
+    );
+  }
 }
