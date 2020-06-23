a04fc06be50dcdf7f72f6f72852235ff1449b519
geekboysupreme <shuvam.manna@gmail.com>
2020-06-23 22:02:15 +0530

    Updating Live Page

--- src/routes/live/index.js
+++ src/routes/live/index.js
@@ -1,4 +1,5 @@
 import { Component } from "preact";
+import IoLogo from "../../components/io_logo";
 import SocialFooter from "../../components/social_footer";
 import Footer from "../../components/footer";
 import style from "./style";
@@ -25,14 +26,36 @@ export default class EventLivePage extends Component {
 
   render({ rootPath, info }) {
     return (
-      <div class={style.scrollbar}>
+        <div class={style.scrollbar}>
         {/* {info && info.map_image && (<img class={style.mapImage} crossorigin="anonymous"  src={info.map_image} />)}
          */}
+        <div class={`${style.hero} hero`}>
+        <IoLogo rootPath={rootPath} />
+					<h2>Watch live</h2>
+					<p>
+					  Stay Tuned here for the Live Sessions, updates and much, much more.
+					<br />
+            Also, subscribe to our YouTube Channel and hit the Bell Icon to get more updates.
+          </p>
+
+          <div class={style.button_holder}>
+              <a
+              href="https://www.youtube.com/DiversityInDSC?reload=9"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button class={style.action_button}>
+                <b>Find us on YouTube</b>
+              </button>
+            </a>
+            </div>
+				</div>
         <div class={style.footer}>
           <SocialFooter rootPath={rootPath} />
           <Footer rootPath={rootPath} />
         </div>
       </div>
+      
     );
   }
 }
